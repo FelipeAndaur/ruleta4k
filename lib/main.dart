36dab8654b760d1deafd61b1d26aff919d47fc0d
FelipeAndaur <felipe@appy.cl>
2024-08-16 08:33:44 -0400

    chore: initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Ruleta Agrosuper',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Ruleta de la Fortuna'),
-        ),
-        body: const Center(
+        body: Center(
           child: FortuneWheelExample(),
         ),
       ),
@@ -33,21 +30,66 @@ class FortuneWheelExample extends StatefulWidget {
 class _FortuneWheelExampleState extends State<FortuneWheelExample> {
   final StreamController<int> _controller = StreamController<int>();
   int selectedIndex = 0;
+  bool isButtonActive = true;
 
-  // Definir una lista con 12 opciones, algunas de las cuales se repiten
-  final List<Map<String, IconData?>> opciones = [
-    {"Siga\nparticipando": null},
-    {"Mandil": Icons.check},
-    {"Tabla\nde sal": Icons.spa},
-    {"Siga\nparticipando": null},
-    {"Pack de\ngalletas": Icons.fastfood},
-    {"Siga\nparticipando": null},
-    {"Cubiertos\nsustentables": Icons.eco},
-    {"Siga\nparticipando": null},
-    {"Tabla\nde sal": Icons.spa},
-    {"Siga\nparticipando": null},
-    {"Pack de\ngalletas": Icons.fastfood},
-    {"Siga\nparticipando": null},
+  // Definir una lista con 12 opciones, cada una con un color de fondo diferente
+  final List<Map<String, dynamic>> opciones = [
+    {"texto": "Mandil", "icono": Icons.check, "color": const Color(0xFF0161d3)},
+    {
+      "texto": "Siga\nparticipando",
+      "icono": null,
+      "color": const Color(0xFF0070E3)
+    },
+    {
+      "texto": "Tabla\nde sal",
+      "icono": Icons.spa,
+      "color": const Color(0xFF0150c6)
+    },
+    {
+      "texto": "Siga\nparticipando",
+      "icono": null,
+      "color": const Color(0xFF013b98)
+    },
+    {
+      "texto": "Pack de\ngalletas",
+      "icono": Icons.fastfood,
+      "color": const Color(0xFF07308d)
+    },
+    {
+      "texto": "Siga\nparticipando",
+      "icono": null,
+      "color": const Color(0xFF0070e3)
+    },
+    {
+      "texto": "Cubiertos\nsustentables",
+      "icono": Icons.eco,
+      "color": const Color(0xFF0161d3)
+    },
+    {
+      "texto": "Siga\nparticipando",
+      "icono": null,
+      "color": const Color(0xFF0056ca)
+    },
+    {
+      "texto": "Tabla\nde sal",
+      "icono": Icons.spa,
+      "color": const Color(0xFF0047b6)
+    },
+    {
+      "texto": "Siga\nparticipando",
+      "icono": null,
+      "color": const Color(0xFF013b98)
+    },
+    {
+      "texto": "Pack de\ngalletas",
+      "icono": Icons.fastfood,
+      "color": const Color(0xFF072e87)
+    },
+    {
+      "texto": "Siga\nparticipando",
+      "icono": null,
+      "color": const Color(0xFF0070e3)
+    },
   ];
 
   @override
@@ -59,28 +101,26 @@ class _FortuneWheelExampleState extends State<FortuneWheelExample> {
   void mostrarResultado(BuildContext context, String resultado) {
     showDialog(
       context: context,
-      barrierDismissible:
-          true, // Permite cerrar el diálogo al tocar fuera de él
+      barrierDismissible: true,
       builder: (BuildContext context) {
         return Dialog(
           insetPadding: const EdgeInsets.all(0),
           backgroundColor: Colors.transparent,
           child: TweenAnimationBuilder(
-            tween: Tween<double>(begin: 0.0, end: 1.0), // Animar desde 0 a 1
-            duration:
-                const Duration(milliseconds: 1500), // Duración de la animación
-            curve: Curves.easeOutBack, // Curva de la animación
+            tween: Tween<double>(begin: 0.0, end: 1.0),
+            duration: const Duration(milliseconds: 1500),
+            curve: Curves.easeOutBack,
             builder: (context, double scale, child) {
               return Transform.scale(
-                scale: scale, // Aplicar la escala animada
+                scale: scale,
                 child: Padding(
                   padding: const EdgeInsets.all(0.0),
                   child: Image.asset(
                     resultado == "¡Sigue intentando!"
-                        ? 'assets/win.png' // Ruta de la imagen para perdedor
-                        : 'assets/win.png', // Ruta de la imagen para ganador
+                        ? 'assets/win.png'
+                        : 'assets/win.png',
                     fit: BoxFit.contain,
-                    width: MediaQuery.of(context).size.width, // Ancho máximo
+                    width: MediaQuery.of(context).size.width,
                   ),
                 ),
               );
@@ -97,7 +137,7 @@ class _FortuneWheelExampleState extends State<FortuneWheelExample> {
 
     return LayoutBuilder(
       builder: (context, constraints) {
-        double size = constraints.maxWidth; // Usar el ancho completo
+        double size = constraints.maxWidth;
 
         return Padding(
           padding: const EdgeInsets.all(20.0),
@@ -114,28 +154,27 @@ class _FortuneWheelExampleState extends State<FortuneWheelExample> {
                       animateFirst: false,
                       selected: _controller.stream,
                       items: opciones.map((opcion) {
-                        String text = opcion.keys.first;
-                        IconData? icon = opcion.values.first;
+                        String texto = opcion['texto'];
+                        IconData? icono = opcion['icono'];
+                        Color color = opcion['color'];
                         return FortuneItem(
                           child: RotatedBox(
                             quarterTurns: 5,
                             child: Padding(
-                              padding: EdgeInsets.all(screenWidth *
-                                  0.04), // Ajustar padding proporcionalmente
+                              padding: EdgeInsets.all(screenWidth * 0.04),
                               child: Column(
                                 mainAxisAlignment: MainAxisAlignment.start,
                                 children: [
-                                  if (icon != null)
+                                  if (icono != null)
                                     Icon(
-                                      icon,
+                                      icono,
                                       color: Colors.white,
-                                      size: screenWidth *
-                                          0.07, // Ajustar tamaño del ícono proporcionalmente
+                                      size: screenWidth * 0.07,
                                     ),
                                   FittedBox(
                                     fit: BoxFit.scaleDown,
                                     child: Text(
-                                      text,
+                                      texto,
                                       textHeightBehavior:
                                           const TextHeightBehavior(
                                         applyHeightToFirstAscent: false,
@@ -144,8 +183,7 @@ class _FortuneWheelExampleState extends State<FortuneWheelExample> {
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
                                         color: Colors.white,
-                                        fontSize: screenWidth *
-                                            0.020, // Ajustar tamaño del texto proporcionalmente
+                                        fontSize: screenWidth * 0.020,
                                       ),
                                       textAlign: TextAlign.center,
                                     ),
@@ -155,17 +193,17 @@ class _FortuneWheelExampleState extends State<FortuneWheelExample> {
                             ),
                           ),
                           style: FortuneItemStyle(
-                            color:
-                                Colors.blue, // Color de fondo de este segmento
-                            borderColor: const Color.fromARGB(255, 17, 0, 198),
-                            borderWidth: screenWidth *
-                                0.005, // Ajustar grosor del borde proporcionalmente
+                            color: color,
+                            borderColor: const Color(0xFF013b98),
+                            borderWidth: 0.9,
                           ),
                         );
                       }).toList(),
                       onAnimationEnd: () {
-                        // Mostrar un mensaje basado en el resultado
-                        String resultado = opciones[selectedIndex].keys.first;
+                        setState(() {
+                          isButtonActive = true;
+                        });
+                        String resultado = opciones[selectedIndex]['texto'];
                         if (resultado.startsWith("Siga")) {
                           mostrarResultado(context, "¡Sigue intentando!");
                         } else {
@@ -179,9 +217,8 @@ class _FortuneWheelExampleState extends State<FortuneWheelExample> {
                           child: Transform.translate(
                             offset: Offset(0, -screenWidth * 0.1),
                             child: Image.asset(
-                              'assets/Pin_ruleta.png', // Ruta de la imagen del indicador
-                              width: screenWidth *
-                                  0.1, // Ajustar tamaño del indicador proporcionalmente
+                              'assets/Pin_ruleta.png',
+                              width: screenWidth * 0.1,
                               height: screenWidth * 0.15,
                             ),
                           ),
@@ -189,25 +226,51 @@ class _FortuneWheelExampleState extends State<FortuneWheelExample> {
                       ],
                     ),
                   ),
-                  // Imagen central sobre la ruleta
                   Positioned(
                     child: Image.asset(
-                      'assets/center.png', // Ruta de la imagen del indicador
-                      width: size * 0.6, // Ajustar tamaño de la imagen central
-                      height: size * 0.6, // Ajustar tamaño de la imagen central
+                      'assets/center.png',
+                      width: size * 0.5,
+                      height: size * 0.5,
                     ),
                   ),
                 ],
               ),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    selectedIndex = Fortune.randomInt(0, opciones.length);
-                    _controller.add(selectedIndex);
-                  });
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return FadeTransition(
+                    opacity: animation,
+                    child: child,
+                  );
                 },
-                child: const Text('Girar'),
+                child: GestureDetector(
+                  key: ValueKey<bool>(isButtonActive),
+                  onTap: isButtonActive
+                      ? () {
+                          setState(() {
+                            selectedIndex =
+                                Fortune.randomInt(0, opciones.length);
+                            _controller.add(selectedIndex);
+                            isButtonActive = false;
+                          });
+                        }
+                      : null,
+                  child: Container(
+                    width: screenWidth * 0.25, // Tamaño fijo para evitar salto
+                    height: screenWidth * 0.25, // Tamaño fijo para evitar salto
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          isButtonActive
+                              ? 'assets/Btn_Girar_Activo.png'
+                              : 'assets/Btn_Girar_Inactivo.png',
+                        ),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
